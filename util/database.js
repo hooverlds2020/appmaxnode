ef3256b9cd0dfdd45b2e61ba0236d350d06ad00a
hooverlds2020 <hooverlds93@gmail.com>
2022-03-09 05:57:43 -0600

    first commit2

--- util/database.js
+++ util/database.js
@@ -20,6 +20,17 @@ const sequelize = new Sequelize({
   }
 });
 
+// Host
+// ec2-52-207-74-100.compute-1.amazonaws.com
+// Database
+// d3p7t9v8spheaq
+// User
+// jxclaphcpoatpf
+// Port
+// 5432
+// Password
+// d931463d8b56110bdd928f261a6d08e166c6f34594e9e78528c2093b06e7f7e4
+
 // Connect to Heroku cloud
 // const sequelize = new Sequelize({
 // 	host: 'heroku_host',
